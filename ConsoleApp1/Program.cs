3d6c1df75abaf724902f7b7dfc1a10b9bf947e14
Sonal Karle <Sonal>
2022-06-06 19:56:52 +0530

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LogicalProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a number to go to the respective problem, \n");
+            Console.WriteLine("1 for Perfect Number");
+            Console.WriteLine("2 for Prime Numer");
+            Console.WriteLine("3 for Reverse a Number");
+
+            int Option = Convert.ToInt32(Console.ReadLine());
+
+            switch (Option)
+            {
+
+                case 1:
+                    Perfect perfect = new Perfect();
+                    perfect.Perfectnum();
+                    break;
+                case 2:
+                    Prime prime = new Prime();
+                    prime.PrimeNum();
+                    break;
+                case 3:
+                    Reverse reverse = new Reverse();
+                    reverse.ReverseNum();
+                    break;
+            }
+        }
+    }
+}
